97e6de46855aa2655198bdac171ddebacf94943e
Owen Morgan <om371@bath.ac.uk>
2017-05-08 15:54:34 +0100

    Done rigid as possible shape deformation.

--- A_gamma_calc.m
+++ A_gamma_calc.m
@@ -2,13 +2,13 @@ function A_out = A_gamma_calc(A,t)
 
 [U,S_SVD,V] = svd(A);
 
-Ry = U*V;
+Ry = U*V';
 Ryz = eye(3);
 Ryz(1:2,1:2) = Ry;
-q0 = Rot2Quat(eye(3));
+q0 = [1,0,0,0];
 q1 = Rot2Quat(Ryz);
-S = V'*S_SVD*V;
-alpha = acos(Ry(1))/2;
+S = V*S_SVD*V';
+alpha = acos(dot(q0,q1))/2;
 
 quat_interp = (q0*sin((1-t)*alpha) + q1*sin(t*alpha))/sin(alpha);
 R_interp = Quat2Rot(quat_interp);
--- rigidinterp.m
+++ rigidinterp.m
@@ -18,28 +18,40 @@ axis([-2, 2, -4, 4]);
 t = 1/(10);
 
 A_gamma = cell(length(vi));
-A = zeros(4*length(vi),2*length(v1));
+P_inv_saved = cell(length(vi));
+A = zeros(4*length(vi),2*(length(v1)-1));
 for i = 1:length(vi)
     p = [v1(vi(i,1),:);v1(vi(i,2),:);v1(vi(i,3),:)];
     q = [v2(vi(i,1),:);v2(vi(i,2),:);v2(vi(i,3),:)];
     [P_inv, a_out] = trerp(p,q);
     A_gamma{i} = a_out;
+    P_inv_saved{i} = P_inv;
     for k = 1:4
         for j = 1:3
-            A(4*i-4+k,2*vi(i,j)-1:2*vi(i,j)) = P_inv(k,2*j-1:2*j);
+            if(vi(i,j)>1)
+                A(4*i-4+k,2*vi(i,j)-3:2*vi(i,j)-2) = P_inv(k,2*j-1:2*j);
+            end
         end
     end
 end
-A_inv = (A'*A)^-1*A';
-b = zeros(4*length(vi),1);
+A_inv = (A'*A)^(-1)*A';
+newV = zeros(2*length(v1),1);
 figure(1);
 hold on;
 for i = 0:t:1
+    newV(1) = (1-i)*v1(1,1) + i*v2(1,1);
+    newV(2) = (1-i)*v1(1,2) + i*v2(1,2);
+    b = zeros(4*length(vi),1);
     for j = 1:length(vi)
-        A_out = A_gamma_calc(A_gamma{j},i);
+        A_out = A_gamma_calc(A_gamma{j},i)';
         b(4*j-3:4*j) = A_out(:);
+        if(nnz(vi(j,:) == 1))
+            idx = find(vi(j,:) == 1);
+            P_inv = P_inv_saved{j};
+            b(4*j-3:4*j) = b(4*j-3:4*j) - P_inv(:,2*idx-1)*newV(1) - P_inv(:,2*idx)*newV(2);
+        end
     end
-    newV = A_inv * b;
+    newV(3:end) = A_inv * b;
     newV = reshape(newV,[2,length(v1)]);
     newV = newV';
     
